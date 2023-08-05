3e6dd05580953a9717ea9999d55705bd615ecc21
Leroy <leroypawlick@gmail.com>
2023-08-05 11:18:08 -0700

    added middleware

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const SECRET = process.env.SECRET
+const jwt = require('jsonwebtoken')
+
 
 const createToken = (username, id) => {
     return jwt.sign({ username, id }, SECRET, { expiresIn: '2 days'})
