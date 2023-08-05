1ccb765e22904e3415f8c82a5141fb91d5164371
Leroy <leroypawlick@gmail.com>
2023-08-05 14:48:17 -0700

    finished register functionality

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const SECRET = process.env.SECRET
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { User } = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 
 const createToken = (username, id) => {
@@ -15,8 +17,28 @@ module.exports = {
     },
 
     register: async (req, res) => {
-        console.log('register');
-        res.sendStatus(200);
+        try {
+            const { username, password } = req.body;
+            const foundUser = await User.findOne({where: {username: username}})
+            if (foundUser) {
+                res.status(400).send('Username already taken!')
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(password, salt);
+                const newUser = await User.create({username: username, hashedPass: hash});
+                const token = createToken(newUser.dataValues.id, newUser.dataValues.username);
+                console.log(newUser);
+                const exp = Date.now() + 1000 * 60 * 60 * 48
+                res.status(200).send({
+                    username: newUser.dataValues.username,
+                    userId: newUser.dataValues.id,
+                    token,
+                    exp,
+                  });
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
