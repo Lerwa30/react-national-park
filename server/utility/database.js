c22a8716ee92ca8f1c23ea4fc1a814086043acd2
Leroy <leroypawlick@gmail.com>
2023-08-05 11:25:03 -0700

    installed sequelize and connected to database

---
+++ server/utility/database.js
@@ -0,0 +1,16 @@
+require('dotenv').configure();
+const CONNECTION_STRING = process.env;
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
+
+module.exports = {
+    sequelize
+};
