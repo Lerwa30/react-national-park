d21c10543dd333f50444137061a246cefe97a4f1
Leroy <leroypawlick@gmail.com>
2023-08-05 09:33:55 -0700

    added server and port, created controller file for backend

--- server/index.js
+++ server/index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const { PORT } = process.env
+
+
+
+
+
+
+app.listen(PORT, () => console.log('Server running'))
