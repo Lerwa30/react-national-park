31ffdbf13650c82db9a6cddc3fd377002cb5c631
Leroy <leroypawlick@gmail.com>
2023-08-05 10:41:06 -0700

    fixxed error on register button

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,24 +1,55 @@
 import { useState, useContext } from "react";
 import axios from "axios";
+import AuthContext from "../../authContext";
 
 const Login = () => {
-    const { register, setRegister} = useState(false);
-    const { username, setUsername} = useState('');
-    const { password, setPassword } = useState('');
+  const [register, setRegister] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  const authCtx = useContext(AuthContext);
 
-    return (
-        <div>
-            <form>
-                <input
-                type="text"
-                placeholder="username"
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-                >
-                </input>
-            </form>
-        </div>
-    )
-}
+    const body = {
+      username,
+      password,
+    };
+
+    axios
+      .post(register ? "/register" : "/login", body)
+      .then((res) => {
+        console.log("AFTER AUTH", res.data);
+        authCtx.login(res.data.token, res.data.exp, res.data.userId);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <button className="form-btn">{register ? "Sign Up" : "Login"}</button>
+      </form>
+      <button className="form-btn" onClick={() => setRegister(!register)}>
+        Need to {register ? "Login" : "Sign Up"}?
+      </button>
+    </div>
+  );
+};
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import AuthContext, { AuthContextProvider } from './authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
