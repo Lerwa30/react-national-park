178f39cb4db963a643bf2c06536f181c0fefd3e2
Leroy <leroypawlick@gmail.com>
2023-08-14 18:39:38 -0700

    fixed random function for api call

--- src/components/elements/ParkCard.js
+++ src/components/elements/ParkCard.js
@@ -4,9 +4,9 @@ import classes from './ParkCard.module.css'
 const ParkCard = (props) => {
   return (
     <div className={classes.container}>
-      <div>{props.park.images === undefined ? <span>loading image</span> : 
+      <div>{props.park.images === undefined ? <span></span> : 
       <img className={classes.img} src={props.park.images[0].url}></img>}
-      <h3>{props.park.fullName}, {props.park.states}</h3>
+      <h3>{props.park.fullName} {props.park.states}</h3>
       <p>{props.park.description}</p>
       </div>
     </div>
