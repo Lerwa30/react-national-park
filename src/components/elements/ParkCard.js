5f8afa8c9916859edb7e68e6c2f8349519e1b8a4
Leroy <leroypawlick@gmail.com>
2023-08-20 11:38:28 -0700

    fixed css for button on header and park card

--- src/components/elements/ParkCard.js
+++ src/components/elements/ParkCard.js
@@ -14,7 +14,7 @@ const ParkCard = ({park}) => {
       <img className={classes.img} src={image} alt='no img'/>}
       <h3>{park.fullName}<br />{park.states}</h3>
       <p className={classes.description}>{park.description}</p>
-      <Link to={`/details/${park.parkCode}`}><button>View Details</button></Link>
+      <Link to={`/details/${park.parkCode}`}><button className={classes.button}>View Details</button></Link>
       </div>
     </div>
   );
