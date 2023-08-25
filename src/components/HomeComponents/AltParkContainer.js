ec00eeb27d448b80759770088a88e9f0fb32c48f
Leroy <leroypawlick@gmail.com>
2023-08-24 20:40:03 -0700

    added ternary in ParkCard to allow images more time to load nefore error when API is called

--- src/components/HomeComponents/AltParkContainer.js
+++ src/components/HomeComponents/AltParkContainer.js
@@ -1,16 +1,73 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import ParkCard from "../elements/ParkCard";
 import classes from "./ParkContainer.module.css";
 
 const AltParkContainer = ({ parks }) => {
-  const stateArr = ['AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND',
-    'MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT',
-    'VT','VI','VA','WA','WV','WI','WY']
+  const stateArr = [
+    "AL",
+    "AK",
+    "AS",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "DC",
+    "FM",
+    "FL",
+    "GA",
+    "GU",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MH",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "MP",
+    "OH",
+    "OK",
+    "OR",
+    "PW",
+    "PA",
+    "PR",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VI",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+  ];
 
   const [selectedState, setSelectedState] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
 
   console.log(parks);
 
@@ -19,17 +76,13 @@ const AltParkContainer = ({ parks }) => {
     .filter((park) => {
       let option = park.states;
       return option.includes(selectedState);
-      
     })
-    useEffect(() => {
-        setIsLoading(false);
-    }, [parkDisplay])
-    .map((park) => {
-      console.log('test')
+    .map((park, index) => {
+      console.log("test");
       return <ParkCard park={park} />;
     });
-    console.log(selectedState);
 
+  console.log(selectedState);
 
   return (
     <section className={classes.section}>
@@ -39,17 +92,16 @@ const AltParkContainer = ({ parks }) => {
           value={selectedState}
           onChange={(e) => {
             setSelectedState(e.target.value);
-          }}>
-                <option value='selectedState'>Select a State</option>
+          }}
+        >
+          <option value="">Select a State</option>
 
-            {stateArr.map((state, index) => {
-            return (
-                <option>{state}</option>
-            )
-          })}</select>
-        </div>
-       (<div className={classes.container}>{parkDisplay}</div>)
-    
+          {stateArr.map((state, index) => {
+            return <option value={state}>{state}</option>;
+          })}
+        </select>
+      </div>
+      <div className={classes.container}> {parkDisplay}</div>
     </section>
   );
 };
--- src/components/elements/ParkCard.js
+++ src/components/elements/ParkCard.js
@@ -1,21 +1,31 @@
 import React from "react";
 import classes from './ParkCard.module.css'
-
-
 import { Link } from "react-router-dom";
 
-const ParkCard = ({park}) => {  
-  console.log(park)
+const ParkCard = ({ park }) => {
+  console.log(park);
+
+  const imageUrl = park.images[0] && park.images[0].url ? park.images[0].url : null; //
 
   return (
     <div className={classes.park_card}>
-      <div className={classes.img_container}>{park.images[0].url === undefined ? <span></span> : 
-      <img className={classes.img} src={park.images[0].url} alt='no img' loading='lazy' />}
-      <h3>{park.fullName}<br />{park.states}</h3>
-      <p className={classes.description}>{park.description}</p>
-      <Link to={`/details/${park.parkCode}`}><button className={classes.button}>View Details</button></Link>
+      <div className={classes.img_container}>
+        {imageUrl ? (
+          <img className={classes.img} src={imageUrl} alt='no img' loading='lazy' />
+        ) : (
+          <span>No Image Available</span>
+        )}
+        <h3>
+          {park.fullName}
+          <br />
+          {park.states}
+        </h3>
+        <p className={classes.description}>{park.description}</p>
+        <Link to={`/details/${park.parkCode}`}>
+          <button className={classes.button}>View Details</button>
+        </Link>
       </div>
-     </div>
+    </div>
   );
 };
 
